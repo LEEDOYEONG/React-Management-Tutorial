0f6e794d02ec85085d886b7f2d1d53da2f3fcc44
이도영 선임 석유화학정보화팀 (scleedo@lgcns.com <78096@CORP.LGCNS.COM>
2020-04-02 10:10:08 +0900

    no_13 file upload

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Customer from './components/Customer';
+import CustomerAdd from './components/CustomerAdd';
 import './App.css';
 import Paper from '@material-ui/core/Paper';
 import Table from '@material-ui/core/Table';
@@ -37,7 +38,7 @@ class App extends Component {
     
     this.callApi()
     .then( res => this.setState({customers : res}))
-    .then(err => console.log(err))
+    .then()
     
     
   }
@@ -56,34 +57,37 @@ class App extends Component {
   render(){
     const {classes} = this.props;
     return(
-      <Paper className={classes.root}>
-        <Table className={classes.table}>
-          <TableHead>
+      <div>
+        <Paper className={classes.root}>
+          <Table className={classes.table}>
+            <TableHead>
+              <TableRow>
+                <TableCell>ID</TableCell>
+                <TableCell>이미지</TableCell>
+                <TableCell>이름</TableCell>
+                <TableCell>생년월일</TableCell>
+                <TableCell>성별</TableCell>
+                <TableCell>직업</TableCell>
+              </TableRow>
+            </TableHead>
+          <TableBody>
+            { this.state.customers ?  this.state.customers.map( (c) =>{
+              return (
+                <Customer key={c.id} id={c.id} image = {c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/>
+              )
+            }) : 
             <TableRow>
-              <TableCell>ID</TableCell>
-              <TableCell>이미지</TableCell>
-              <TableCell>이름</TableCell>
-              <TableCell>생년월일</TableCell>
-              <TableCell>성별</TableCell>
-              <TableCell>직업</TableCell>
+              <TableCell colSpan="6" align = "center">
+                <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed}/>
+              </TableCell>
             </TableRow>
-          </TableHead>
-         <TableBody>
-          { this.state.customers ?  this.state.customers.map( (c) =>{
-             return (
-               <Customer key={c.id} id={c.id} image = {c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/>
-             )
-           }) : 
-           <TableRow>
-             <TableCell colSpan="6" align = "center">
-               <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed}/>
-             </TableCell>
-           </TableRow>
-          }
-         </TableBody>
-        </Table>
-      
-      </Paper>
+            }
+          </TableBody>
+          </Table>
+        </Paper>
+         <CustomerAdd/>
+      </div>
+     
     )
   }
 }
--- server.js
+++ server.js
@@ -15,20 +15,38 @@ const connection = mysql.createConnection({
   host : conf.host,
   user : conf.user,
   password : conf.password,
-  port : conf.port,
+  port : conf.port, 
   database : conf.database
 });
 
 connection.connect();
 
+const multer = require("multer");
+const upload = multer({dest : "./upload"})
+
 app.get("/api/customers", (req, res) => {
   connection.query(
     "SELECT * FROM CUSTOMER",
     (err, rows, fiels) => {
-      console.log(err);
       res.send(rows);
     }
   );
 });
+
+app.use('/image', express.static('./upload'));
+app.post('/api/customers', upload.single('image'), (req, res)=>{
+  let sql = 'INSERT INTO CUSTOMER VALUES (NULL, ?, ?, ?, ?, ?)';
+  let image = '/image/' + req.file.filename;
+  let name = req.body.name;
+  let birthday = req.body.birthday;
+  let gender = req.body.gender;
+  let job = req.body.job
+  let params = [image, name, birthday, gender, job];
+  connection.query(sql, params,
+  (err, rows, fields) => {
+    console.log(err);
+    res.send(rows);
+  })
+})
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
